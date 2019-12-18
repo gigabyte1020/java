7afd3963fb439a62a3e2f62802b0a10f1a45c014
gigabyte1020 <55729626+gigabyte1020@users.noreply.github.com>
2019-12-18 15:32:15 +0530

    Add files via upload

---
+++ ConnectionCls.java
@@ -0,0 +1,67 @@
+package test;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ConnectionCls 
+{
+     String url = "jdbc:mysql://localhost:3306/health";
+    String driver = "com.mysql.jdbc.Driver";
+   String userName = "root"; 
+    String password = "";
+    public Statement stmt = null;
+    public  Connection conn = null;
+    public ResultSet rs = null;
+    
+    
+    public String InsertUpdate(String query)
+    {
+        try
+        {
+            Object newInstance = Class.forName(driver).newInstance();
+            conn = DriverManager.getConnection(url,userName,password);
+            stmt=conn.createStatement();
+            int i=stmt.executeUpdate(query);
+            conn.close();
+            stmt.close();
+            if(i==1)
+            {
+               
+              return "success";
+                  
+            }
+            else
+            {
+               return "Failed";
+            }
+          
+        }
+       catch(Exception ex)
+        {
+           return ex.toString();
+       }
+    }
+     public ResultSet getDetails(String query)
+    {
+              try
+              {
+                Class.forName(driver).newInstance();
+                conn = DriverManager.getConnection(url,userName,password);
+                stmt=conn.createStatement();
+                rs=stmt.executeQuery(query);
+              }
+              catch(Exception ex)
+              {
+                  ex.printStackTrace();
+              }
+              return rs;
+    }
+}
